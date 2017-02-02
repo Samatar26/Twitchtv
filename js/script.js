760a7c111be5597672a9a7036ea83b014af9958e
samatar axmed <Samatar_267@hotmail.com>
2017-02-02 12:47:55 +0000

    Finish displaying divs for online, offline and disbanded users

--- js/script.js
+++ js/script.js
@@ -2,35 +2,131 @@ window.onload = function() {
 
     var xhr = new XMLHttpRequest();
     var section = document.getElementById("container");
-    var article = document.createElement("article");
-    var logo = document.createElement("img");
-    var username = document.createElement("h2");
+
+
+
+
+
+
 
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200){
 
+
             var json = JSON.parse(this.responseText);
-            console.log(json);
+            if(json.stream===null){
+                var xhrTwo = new XMLHttpRequest();
+                xhrTwo.onreadystatechange = function(){
+
+                    if(this.readyState == 4 && this.status == 200) {
+                      var jsonTwo = JSON.parse(xhrTwo.responseText);
+                      //check user exists - null
+                      console.log(jsonTwo)
+                      if(jsonTwo.mature===null){
+                        var article = document.createElement("article");
+                        var status = document.createElement("p");
+                        status.innerText = "Account " + jsonTwo.display_name +  " does not exist";
+                        status.id = "disbanded";
+                        article.appendChild(status);
+                        section.appendChild(article);
+
+
+
+
+                      }
+                      else {
+                        //username
+
+                        var usernameText = document.createTextNode(jsonTwo.display_name);
+                        var username = document.createElement("h2");
+                        username.appendChild(usernameText);
+                        username.id = "username";
+                        var article = document.createElement("article");
+                        article.appendChild(username);
+                        article.id = "esl";
+                        section.appendChild(article);
+                        //logo
+                        var logo = document.createElement("img");
+                        logo.src = jsonTwo.logo;
+                        logo.id="logo";
+                        article.appendChild(logo);
+                        //status
+                        var statusOuput = document.createTextNode("Offline");
+                        var status = document.createElement("p");
+                        var anchorLink = document.createElement("a");
+                        anchorLink.href=jsonTwo.url;
+                        status.appendChild(statusOuput);
+                        anchorLink.appendChild(status);
+                        status.id = "offline";
+                        article.appendChild(anchorLink);
+
+
+
+
+                      }
+
+
+                    }
+
+
+                }
+
+                xhrTwo.open("GET", "https://wind-bow.gomix.me/twitch-api/channels" + "/" + j[i], false);
+                xhrTwo.send();
+
+            }
+            else{
             //esl_sc2
             //username
+
             var usernameText = document.createTextNode(json.stream.channel.display_name);
+            var username = document.createElement("h2");
             username.appendChild(usernameText);
+            username.id = "username";
+            var article = document.createElement("article");
             article.appendChild(username);
+            article.id = "esl";
             section.appendChild(article);
             //logo
-            logo.setAttribute("src", json.stream.channel.logo);
+            var logo = document.createElement("img");
+            logo.src = json.stream.channel.logo;
+            logo.id="logo";
             article.appendChild(logo);
             //Status output
             var statusOuput = document.createTextNode(json.stream.channel.status);
-            username.
+            var status = document.createElement("p");
+            var anchorLink = document.createElement("a");
+            anchorLink.href=json.stream.channel.url;
+            status.appendChild(statusOuput);
+            anchorLink.appendChild(status);
+            status.id = "status";
+            article.appendChild(anchorLink);
+            console.log(status);
+            //Preview Image
+            var previewImage = json.stream.preview.medium;
+            var preview = document.createElement("img");
+            preview.src=previewImage;
+            preview.id = "preview";
+            console.log(previewImage);
+            article.appendChild(preview);
+            //Viewers
+            var viewersApi = document.createTextNode(json.stream.viewers+" viewers");
+            var viewers = document.createElement("p");
+            viewers.appendChild(viewersApi);
+            viewers.id = "viewers";
+            article.appendChild(viewers);
+          }
+
 
 
           }
     }
-
-    xhr.open("GET", "https://wind-bow.gomix.me/twitch-api/streams/esl_sc2", false);
+    for (var i = 0; i<4; i++){
+    var j = ["esl_sc2", "OgamingSC2", "freecodecamp", "ssssssssssssss"];
+    xhr.open("GET", "https://wind-bow.gomix.me/twitch-api/streams/"+j[i], false);
     xhr.send();
 
+  }
 
 
 
